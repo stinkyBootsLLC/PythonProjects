3cdd57d3c4fa8e2e854037f724380436dd0fb69a
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2022-12-09 20:44:25 -0500

    seperated each dir

--- adventCode/day7/day07.py
+++ adventCode/day7/day07.py
@@ -8,15 +8,16 @@
  
       
 """
- 
+import re
 
 with open('input.txt', 'r', encoding="utf-8") as f:
     data = {}
+    drive = {}
+    sum_of_all_files = 0
    
     lines = f.readlines()
     # parse input file
     for line in lines:
-        
         
         if "$ cd" in line.strip():
 
@@ -30,12 +31,42 @@ with open('input.txt', 'r', encoding="utf-8") as f:
                 # print(line.strip())
                 data[location].append(line.strip())
              
-        
-            
-
-
-    print(data)
     # determine the total size of each directory 
+    for key, values in data.items():
+
+        drive[key] = []
+        sum_of_dir = 0
+
+        for value in values:
+            
+            found_file = any(char.isdigit() for char in value)
+            # found a file
+            if found_file:
+                # then get the size and name
+                size, name = value.split(" ")
+                sum_of_dir += int(size)
+                
+            else:
+            # else its a directory
+                # print(key + value[-1])
+                drive[key].append(value[-1])
+        
+        drive[key].append(sum_of_dir)
+        sum_of_all_files += sum_of_dir
+        
+    
+    
+    # determine the total size of each directory
+    print(data)
+    print(drive)
+    print(sum_of_all_files)
+        
+                
+     
+
+
+
+
     # 
     # The total size of a directory is the sum of 
     # the sizes of the files it contains, directly or indirectly   
