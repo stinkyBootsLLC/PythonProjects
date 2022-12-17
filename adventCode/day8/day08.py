0f78a3f910a88ed13c4b03c6bd8260b4936b2e53
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2022-12-17 14:09:02 -0500

    adding

--- adventCode/day8/day08.py
+++ adventCode/day8/day08.py
@@ -8,34 +8,34 @@ def get_highest_scenic_score(lines):
     columns = len(grid[0])
 
     grid = np.array(grid)
+    # right, left, down, up
+    movement_directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-    dd = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-    ans = 0
+    scenic_score = 0
     for i in range(rows):
         for j in range(columns):
             height = grid[i, j]
             score = 1
 
             # Scan in 4 directions
-            for di, dj in dd:
-                ii, jj = i + di, j + dj
-                dist = 0
+            for direction_i, direction_j in movement_directions:
+                ii, jj = i + direction_i, j + direction_j
+                distance = 0
 
                 while (0 <= ii < rows and 0 <= jj < columns) and grid[ii, jj] < height:
-                    dist += 1
-                    ii += di
-                    jj += dj
+                    distance += 1
+                    ii += direction_i
+                    jj += direction_j
 
                     if (0 <= ii < rows and 0 <= jj < columns) and grid[ii, jj] >= height:
-                        dist += 1
+                        distance += 1
 
-                score *= dist
+                score *= distance
 
-            ans = max(ans, score)
+            scenic_score = max(scenic_score, score)
 
 
-    print(ans)    
+    return scenic_score   
 
 
 
@@ -84,3 +84,6 @@ with open("input.txt", "r") as file_in:
 # how many trees are visible from outside the grid?
 p1_ans = get_visible_trees_from_outside_grid(lines)
 print(p1_ans)
+# What is the highest scenic score possible for any tree?
+p2_ans = get_highest_scenic_score(lines)
+print(p2_ans)
--- adventCode/day8/readme.md
+++ adventCode/day8/readme.md
@@ -1,4 +1,7 @@
 https://adventofcode.com/2022/day/8
+
+Answer by https://github.com/womogenes
+
 ### --- Day 8: Treetop Tree House ---
 
 ### --- Part One ---
@@ -8,18 +11,14 @@ get_visible_trees_from_outside_grid():<br>
 Returns visible_trees{int}
  
 Ingest puzzle input 
-
 ```
 grid = [list(map(int, list(line))) for line in lines]
 ```
-
 Use NumPy to create a 2D Array 
-
 ```
 grid = np.array(grid)
 ```
 Loop through grid 
- 
 ```
 # All of the trees around the edge of the grid are visible 
 
@@ -33,5 +32,15 @@ elif i == 0 or np.amax(grid[:i, j]) < height:
 elif i == rows - 1 or np.amax(grid[(i+1):, j]) < height:
 ```
 ### --- Part Two ---
+What is the highest scenic score possible for any tree?
+
+get_highest_scenic_score():<br>
+Returns scenic_score{int}
+
+List of directions
+```
+# right, left, down, up
+movement_directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+```
 
 
