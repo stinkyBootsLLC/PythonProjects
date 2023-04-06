700225ad41711930c347bb55c67c62e4911ab690
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-04-06 09:14:09 -0400

    added docs and final cleanup

--- nextcloud/upload.py
+++ nextcloud/upload.py
@@ -6,45 +6,74 @@ import sys
 
 
 def upload_files(nextcloud_client, logger):
+    """ Uploads files to nextcloud instance
+
+    Parameters
+    ----------
+    nextcloud_client : nextcloud_client
+        The nextcloud client object
+    logger : logging
+        The logging object
+    """
 
     print("Uploading files ..............")
 
-    # Get the list of all files and directories
-    files_to_upload = os.listdir(config.local_path)
-    
-    for file in files_to_upload:
+    try:
+        # Get the list of all files and directories
+        files_to_upload = os.listdir(config.local_path)
+        
+        for file in files_to_upload:
 
-        nextcloud_client.put_file(config.remote_path + file, config.local_path + file)
-        logger.info('Uploaded - ' + file) 
-        print('Uploaded - ' + file)
-        # link_info = nc.share_file_with_link('AutoLoaded/' + file)
-        # print("Here is your link: " + link_info.get_link())
+            nextcloud_client.put_file(config.remote_path + file, config.local_path + file)
+            logger.info('Uploaded - ' + file) 
+            print('Uploaded - ' + file)
+            # link_info = nc.share_file_with_link('AutoLoaded/' + file)
+            # print("Here is your link: " + link_info.get_link())
 
-    logger.info("complete") 
-    print('complete')
+        logger.info("upload complete") 
+        
+    except:
+        # log error and exit
+        logger.error("files NOT uploaded")
+        sys.exit("ERROR: files NOT uploaded")
     
 
 def login_remote_location(username, password, nextcloud_client, logger):
+    """ Logs into nextcloud instance
+    
+    Parameters
+    ----------
+    username : str
+        The user's username
+    password : str
+        The user's password
+    nextcloud_client : nextcloud_client
+        The nextcloud client object
+    logger : logging
+        The logging object
+    """
 
     print("Login into nextcloud ..............")
 
     try:
+        # log into account
         nextcloud_client.login(username, password)
-
+        print('login complete')
+        # upload files
         upload_files(nextcloud_client, logger)
+        print('upload complete')
 
     except:
+        # log error and exit
         logger.error("could not login for " + username)
-        sys.exit("could not login for " + username)
-
-
-
+        sys.exit("ERROR: could not login for " + username)
    
 def main():
-    """
-    Main entry to application
+    """ Main entry to application
+
     """
 
+    # set logger configuration
     logging.basicConfig(filename='script.log', filemode='a',
     format='%(asctime)s - %(msecs)d - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d - %H:%M:%S',level=logging.INFO)
@@ -52,9 +81,9 @@ def main():
     logger = logging.getLogger() 
     # Set the threshold of logger to DEBUG 
     logger.setLevel(logging.DEBUG) 
-    # create the client object 
+    # create the nextcloud client object 
     nc = nextcloud_client.Client(config.next_cloud_location)
-
+    # call function
     login_remote_location(config.username, config.password, nc, logger)
     # nc.mkdir('AutoLoaded')
 
