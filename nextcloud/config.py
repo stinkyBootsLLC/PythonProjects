c9efb670a68cfa0631b3f9e7ae4387a06c5d5049
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-04-05 20:11:47 -0400

    added logging

---
+++ nextcloud/config.py
@@ -0,0 +1,5 @@
+username = "eponch@yahoo.com"
+password = "Gofuckyourself"
+next_cloud_location = 'http://150.136.56.126:8080/'
+local_path = "/Users/eduardo/Desktop/AutoLoaded/"
+remote_path = "AutoLoaded/"
--- nextcloud/upload.py
+++ nextcloud/upload.py
@@ -4,6 +4,14 @@
 import nextcloud_client
 import os
 import config
+import logging
+
+
+logging.basicConfig(filename="script.log", format='%(asctime)s %(message)s', filemode='a') 
+# Create an object 
+logger=logging.getLogger() 
+# Set the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
  
 nc = nextcloud_client.Client(config.next_cloud_location)
 nc.login(config.username, config.password)
@@ -14,8 +22,10 @@ files_to_upload = os.listdir(config.local_path)
 for file in files_to_upload:
 
     nc.put_file(config.remote_path + file, config.local_path + file)
+    logger.info('Uploaded - ' + file) 
     print('Uploaded - ' + file)
     # link_info = nc.share_file_with_link('AutoLoaded/' + file)
     # print("Here is your link: " + link_info.get_link())
 
+logger.info("complete") 
 print('complete')
