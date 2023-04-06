25fbcfa754fb39922a26967ed3b0fbd268c3caa3
stinkyBootsLLC <stinkybootsllc@gmail.com>
2023-04-05 20:12:09 -0400

    Delete config.py

--- nextcloud/config.py
+++
@@ -1,5 +0,0 @@
-username = "eponch@yahoo.com"
-password = "Gofuckyourself"
-next_cloud_location = 'http://150.136.56.126:8080/'
-local_path = "/Users/eduardo/Desktop/AutoLoaded/"
-remote_path = "AutoLoaded/"
