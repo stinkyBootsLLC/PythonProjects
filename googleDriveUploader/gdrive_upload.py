fc68c07ded665461730e83242a123504ce520711
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-06-23 10:46:06 -0400

    removed

--- googleDriveUploader/gdrive_upload.py
+++
@@ -1,4 +0,0 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-# @TODO create this first - > https://d35mpxyw7m7k7g.cloudfront.net/bigdata_1/Get+Authentication+for+Google+Service+API+.pdf
