8b09508cd00e7a9545a027934232589420cea659
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-07-28 19:06:54 -0400

    sdfasdfasdf

--- cohort/create_svn_repo.py
+++ cohort/create_svn_repo.py
@@ -6,9 +6,9 @@ def get_user_input():
     repo_name = input("Enter Repository Name: ")
     # get group name 
     group_name = input("Enter Group Name: ")
-    print(f'You entered')
-    print(f'Repository Name: {repo_name.lower()}')
-    print(f'Group Name: {group_name.lower()}')
+    print(f'\nYou entered')
+    print(f'Repository Name: {repo_name}')
+    print(f'Group Name: {group_name}\n')
 
 
  
@@ -25,11 +25,17 @@ def get_user_input():
 # set new dir user and group
 
 if __name__ == "__main__": 
-    print("SVN Repository Creator (v.1.0.1)..............") 
+    print()
+    print("............................................................")
+    print("............................................................")
+    print("..............SVN Repository Creator (v.1.0.0)..............") 
+    print("............................................................")
+    print("............................................................")
+    print()
 
     while True:
         get_user_input()
-        correct = input("Is this information correct? (Y or N)")
+        correct = input("Is this information correct? (Y or N) ")
         if correct.lower() == 'y':
             print('break loop and create repo')
             break
