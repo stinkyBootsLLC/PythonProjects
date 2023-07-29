0d91c0f84053fd72f46570f66d99a661b6e5785f
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2023-07-29 12:33:35 -0400

    ready for testing phase 1

--- cohort/create_svn_repo.py
+++ cohort/create_svn_repo.py
@@ -1,26 +1,47 @@
 import os
 import shutil
+import subprocess
+
+# Set the specified permissions for the directory
+def set_permissions(directory_path):
+   
+    dir_command = 'chmod 775 $(find ' + directory_path + ' -type d)'
+    file_command = 'chmod 666 $(find ' + directory_path + ' -type f)'
+
+    os.system(dir_command)
+    os.system(file_command)
+
 
 # create new dir
-def create_repo(repo_details):
+def create_directory(repo_details):
+
     is_created = False
+
+    # gid = grp.getgrnam(repo_details['group']).gr_gid
+    user = repo_details['group']
+    group = repo_details['group']
+
     # Parent Directory path
     parent_dir = "/Users/eduardo/Desktop/SVNarea"
     # Path
     path = os.path.join(parent_dir, repo_details['repo'])
     try:
         if os.path.exists(path):
-            os.rmdir(path)
-      
             # # Delete Folder code
-            # shutil.rmtree(folderPath)
-            os.mkdir(path) 
+            # os.rmdir(path) will not delete if there are items inside of it
+            shutil.rmtree(path)
+            os.mkdir(path)
+            set_permissions(path, dir_permissions) 
+            shutil.chown(path, user, group)
             is_created = True
   
             print("The folder has been deleted successfully!")
         else:
             os.mkdir(path) 
+            set_permissions(path, dir_permissions)
+            shutil.chown(path, user, group)
             is_created = True
+
     except OSError as error: 
         print(error)
 
@@ -41,19 +62,6 @@ def get_user_input():
     return {"repo": repo_name.lower(), "group": group_name}
 
 
- 
-        
-
-
-
-
-
-
-
-# set new dir perm
-
-# set new dir user and group
-
 if __name__ == "__main__": 
     print()
     print("............................................................")
@@ -68,7 +76,7 @@ if __name__ == "__main__":
         correct = input("Is this information correct? (Y or N) ")
         if correct.lower() == 'y':
             
-            if create_repo(repo_details):
+            if create_directory(repo_details):
                 print('created')
 
             break
